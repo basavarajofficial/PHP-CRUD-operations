747738d415f7f3b5d82c0781ab0502e2668b60fe
basavarajofficial <109651915+basavarajofficial@users.noreply.github.com>
2022-08-14 10:19:28 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$con = new mysqli("sql204.epizy.com","epiz_32241735","0M3GdOI6ia", "epiz_32241735_crudoperation");
+
+if(!$con){
+    die(mysqli_error($con)); //error
+}
+ /*else{
+    die(mysqli_error($con));
+}*/
+?>
